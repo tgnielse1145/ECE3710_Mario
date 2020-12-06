11b5d802fbf2ea75763ea32bc01fbe4c5d31ef01
tgnielse1145 <tgnielse1145@gmail.com>
2020-12-06 13:36:28 -0700

    Update mariokart_top_level.v

--- mariokart_top_level.v
+++ mariokart_top_level.v
@@ -1,30 +1,37 @@
 module mariokart_top_level (
 
-input clock, reset,
+input clock, reset, up_button, down_button, left_button, right_button,
 output vga_clk, vga_blank_n, vga_vs, vga_hs,
 output [7:0] r, g, b
 
 );
-//wire clock1hz;
-//clock_divider divider1(.clock50mHz(clock50mHz), .reset(reset), .enable(1'b1), .clock1Hz(clock1Hz));
-wire [15:0] sys_data;
-wire[15:0] sys_addr;
-wire[15:0]button_in;
-wire[15:0] accel_in;
 
+wire clock50Hz;
+clock_divider divider1(.clock50mHz(clock), .reset(reset), .enable(1'b1), .clock50Hz(clock50Hz));
+wire [15:0] sys_data;
+wire [15:0] sys_addr;
 wire [15:0] alu_bus;
 wire [4:0] flags;
-wire [15:0] addr,  a_in, b_in, opcode;
-wire ls_ctrl, flags_en, regfile_en, alu_mux, pc_en, ir_en, pc_mux, j_en,button_mux, accel_mux;
+wire [15:0] addr,  a_in, b_in, opcode, button_in, accel_in;
+wire ls_ctrl, flags_en, regfile_en, alu_mux, pc_en, ir_en, pc_mux, j_en, button_mux, accel_mux;
+
+assign button_in[15:2] = 14'd0;
+assign button_in[1] = ~up_button;
+assign button_in[0] = ~down_button;
+
+assign accel_in[15] = ~left_button;
+assign accel_in[14:0] = ~$signed(right_button);
 
 localparam SYS_DATA_WIDTH=16, SYS_ADDR_WIDTH=16;
 localparam GLYPH_DATA_WIDTH=24, GLYPH_ADDR_WIDTH=14;
 
 
-wire [(SYS_DATA_WIDTH-1):0] data_a, data_b;
-wire [(SYS_ADDR_WIDTH-1):0] addr_a, addr_b, q_a, q_b;
+wire [(SYS_DATA_WIDTH-1):0] data_a, data_b, q_a, q_b;
+wire [(SYS_ADDR_WIDTH-1):0] addr_a, addr_b;
 wire we_a, we_b;
 
+assign we_b = 1'b0;
+assign data_b = 'b0;
 
 defparam vga.SYS_DATA_WIDTH = SYS_DATA_WIDTH;
 defparam vga.SYS_ADDR_WIDTH = SYS_ADDR_WIDTH;
@@ -33,11 +40,10 @@ defparam vga.GLYPH_ADDR_WIDTH = GLYPH_ADDR_WIDTH;
 localparam COUNT = 12500000;	// 500 sec
 
 
-
 vga vga (
 	.clk(clock),
    .reset(~reset),									
-	.sys_data(a_in),   
+	.sys_data(q_b),   
 	.vga_clk(vga_clk),
 	.vga_blank_n(vga_blank_n),
 	.vga_vs(vga_vs),
@@ -45,8 +51,7 @@ vga vga (
 	.r(r),
 	.g(g),
 	.b(b),											
-	.sys_addr(addr_b)	
-   
+	.sys_addr(addr_b)
 );
 
 
@@ -65,7 +70,6 @@ bram ram (
 	.q_b(q_b)				
 );
 
-
 program_counter pc1(
 	.clock(clock),
 	.reset(reset),
@@ -75,7 +79,7 @@ program_counter pc1(
 	.disp(opcode[7:0]),
 	.j_en(j_en),
 	.dest(b_in)
-	);
+);
 
 assign addr_a = ls_ctrl ? b_in : addr;
 
@@ -91,11 +95,10 @@ datapath dp1(
 	.alu_bus(alu_bus),
 	.a_in(a_in),
 	.b_in(b_in),
-	.button_in(button_in),
-   .accel_in(accel_in),
 	.button_mux(button_mux),
-   .accel_mux(accel_mux)
-
+	.accel_mux(accel_mux),
+	.button_in(button_in),
+	.accel_in(accel_in)
 );
 
 register instruction_register(
@@ -105,7 +108,6 @@ register instruction_register(
 	.en(ir_en),
 	.reset(reset)
 );
-
 
 
 fsm_v3 decoder(
@@ -121,7 +123,9 @@ fsm_v3 decoder(
 	.ir_en(ir_en),
 	.flags_en(flags_en),
 	.pc_mux(pc_mux),
-	.j_en(j_en)
+	.j_en(j_en),
+	.button_mux(button_mux),
+	.accel_mux(accel_mux)
 );
 
 endmodule
