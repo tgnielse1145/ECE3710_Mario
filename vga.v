0841d76a2a6bf0ce9cc707bc450f21894f696293
tgnielse1145 <tgnielse1145@gmail.com>
2020-12-06 13:36:59 -0700

    Update vga.v

--- vga.v
+++ vga.v
@@ -4,25 +4,9 @@ module vga
 (
 	input clk, reset,
 	input [(SYS_DATA_WIDTH-1):0] sys_data,
-	
 	output vga_clk, vga_blank_n, vga_vs, vga_hs,
 	output [7:0] r, g, b,
 	output [(SYS_ADDR_WIDTH-1):0] sys_addr
-	
-);
-
-localparam COUNT = 12500000;	// 500 sec
-//localparam COUNT = 10;
-localparam STATE_ITER = 3;
-wire timer;
-wire [31:0]bitgenCounter;
-
-// clk divider
-pulse clk_div (
-	clk, reset,
-	STATE_ITER, COUNT,
-	timer,
-	bitgenCounter
 );
 
 // size of registers log2(800) ~ 10
@@ -39,14 +23,11 @@ wire [(LOG2_DISPLAY_HEIGHT-1):0] vcount;
 wire [(GLYPH_ADDR_WIDTH-1):0] glyph_addr;
 wire [(GLYPH_DATA_WIDTH-1):0] pixel;
 wire [23:0] bg_color;
-wire [23:0] bg_color1;
 wire [23:0] grid_color;
 wire [23:0] track_color;
+wire [23:0] bound_color;
 wire pix_en;
 reg  pix_en_out;
-wire left;
-wire right;
-wire counter;
 
 /***************************************************************************/
 /* 		VGA CONTROL																			*/
@@ -66,26 +47,30 @@ vga_control ctrl (
 /***************************************************************************/
 /* 		BITGEN																				*/
 /***************************************************************************/
-
+wire res;
 // generate dff to stall and propogate the pix_en 1 cycle to display proper pixels
-//always @(posedge clk)
-//	pix_en_out <= pix_en;
-//counter =0;
 always @(posedge clk)
 	pix_en_out <= pix_en;
 	
 defparam bitgen.DATA_WIDTH = GLYPH_DATA_WIDTH;
+Pixel_On_Text2 #(.displayText("Pixel_On_Text2 -- test1 at (200,200)")) t1(
+                vga_clk,
+                200, // text position.x (top left)
+                200, // text position.y (top left)
+                hcount, // current position.x
+                vcount, // current position.y
+                res  // result, 1 if current pixel is on text, 0 otherwise
+            );
 
 bitgen bitgen (
 	vga_blank_n, pix_en_out,	// inputs  (1-bit)
 	pixel, 							// inputs  (GLYPH_DATA_WIDTH-bit)
-	bg_color,
-   grid_color,
-   track_color,
-   hcount, 
-   vcount,	// inputs  (24-bit)
-	{r,g,b},
-   bitgenCounter	// outputs (24-bit)
+	bg_color, 						// inputs  (24-bit)
+	grid_color, 					// inputs  (24-bit)
+	track_color,					// inputs  (24-bit)
+	bound_color,					// inputs  (24-bit)
+	hcount, vcount,				// inputs (10-bit)
+	{r,g,b}							// outputs (24-bit)
 );
 
 
@@ -104,29 +89,28 @@ glyph_addr_gen addr_gen (
 	clk, reset, vga_blank_n, vga_vs, vga_hs,	// inputs (1-bit)
 	hcount, 												// inputs (LOG2_DISPLAY_WIDTH-bit)
 	vcount,												// inputs (LOG2_DISPLAY_HEIGHT-bit)
-	sys_data,
- 	// outputs (SYS_DATA_WIDTH-bit)
+	sys_data,											// outputs (SYS_DATA_WIDTH-bit)
 	glyph_addr,											// outputs (GLYPH_ADDR_WIDTH-bit)
-	sys_addr,
-  	// outputs (SYS_ADDR_WIDTH-bit)
-	bg_color,
-   grid_color,
-   track_color,	// outputs (24-bit)
+	sys_addr,											// outputs (SYS_ADDR_WIDTH-bit)
+	
+	bg_color,											// outputs (24-bit)
+	grid_color,											// outputs (24-bit)
+   track_color,										// outputs (24-bit)
+	bound_color,										// outputs (24-bit)
 	pix_en												// outputs (1-bit)
 );
 
 /***************************************************************************/
 /* 		GLYPH ROM																			*/
-/***************************************************************************/
+/**************************************************************************/
 
 defparam glyphs.DATA_WIDTH = GLYPH_DATA_WIDTH;
 defparam glyphs.ADDR_WIDTH = GLYPH_ADDR_WIDTH;
 
 glyph_rom glyphs (
 	glyph_addr,		// inputs  (GLYPH_ADDR_WIDTH-bit)
-	clk, 				// inputs  (1-bit)
-	pixel				// outputs (GLYPH_DATA_WIDTH-bit)
+	clk, 					// inputs  (1-bit)
+	pixel				// outputs (GLYPH_DATA_WIDTH-bit)	
 );
-
 
 endmodule
