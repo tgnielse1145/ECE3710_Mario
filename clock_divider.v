f3e4f189d54d0cc385027ee82a55c51d842c6785
tgnielse1145 <tgnielse1145@gmail.com>
2020-12-06 13:43:44 -0700

    Update clock_divider.v

--- clock_divider.v
+++ clock_divider.v
@@ -1,8 +1,8 @@
-module clock_divider(clock50mHz, reset, enable, clock1Hz);
+module clock_divider(clock50mHz, reset, enable, clock50Hz);
 
 input clock50mHz, reset, enable;
 
-output reg clock1Hz;
+output reg clock50Hz;
 
 reg [24:0] count;
 
@@ -10,14 +10,14 @@ always @(posedge clock50mHz, negedge reset) begin
 
 	if (reset == 0) begin
 		count <= 25'd0;
-		clock1Hz <= 0;
+		clock50Hz <= 0;
 	end
 	else if (enable == 0) begin
 		count <= count;
 	end
-	else if (count == 25000000) begin
+	else if (count == 500000) begin
 		count <= 25'd0;
-		clock1Hz <= ~clock1Hz;
+		clock50Hz <= ~clock50Hz;
 	end
 	else begin
 		count <= count + 25'd1;
